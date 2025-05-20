f2da46a53b2cd2607a7d571a6e10f7c9476433de
Kingjibes <160682090+Kingjibes@users.noreply.github.com>
2025-05-20 15:25:49 +0000

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+    import React from 'react';
+    import { Toaster } from '@/components/ui/toaster';
+    import EmailGeneratorPage from '@/pages/EmailGeneratorPage';
+
+    function App() {
+      return (
+        <>
+          <EmailGeneratorPage />
+          <Toaster />
+        </>
+      );
+    }
+
+    export default App;
+  
